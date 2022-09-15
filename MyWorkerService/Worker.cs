acfb7ee76484139150d1e97836b96f765d319e4e
Karthikeyan N <karthikeyan.n@agiliztech.com>
2022-09-15 23:20:56 +0530

    Moved Connection.On to constructor as it was getting called and repeating the server sent messages multiple times

--- MyWorkerService/Worker.cs
+++ MyWorkerService/Worker.cs
@@ -12,9 +12,20 @@ namespace MyWorkerService
             _logger = logger;
             connection = new HubConnectionBuilder().WithUrl("https://localhost:7076/informHub").Build();
             connection.StartAsync().Wait();
-            connection.KeepAliveInterval = TimeSpan.FromDays(100);
-            connection.Closed(() = > { });
+            connection.KeepAliveInterval = TimeSpan.FromSeconds(10);
+            connection.Closed += Connection_Closed;
+            connection.On<string>("Receive", (res) => { _logger.LogInformation(res); });
             connection.InvokeAsync("JoinGroup", "MyGroup").Wait();
+        }
+        
+
+        private Task Connection_Closed(Exception? arg)
+        {
+            connection.StopAsync().Wait();
+            _logger.LogInformation("SignalR connection closed at: {time}. Attempting to restart connection..", DateTimeOffset.Now);
+            connection.StartAsync().Wait();
+            connection.InvokeAsync("JoinGroup", "MyGroup").Wait();
+            return Task.CompletedTask;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -23,8 +34,6 @@ namespace MyWorkerService
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
-
-                connection.On<string>("Receive", (res) => { _logger.LogInformation(res); });
             }
         }
     }
